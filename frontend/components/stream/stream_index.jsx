21f37ae6b8cc032955c9c6751b1a505fae71a691
Winber Xu <winber1@gmail.com>
2017-07-12 16:32:02 -0700

    minor fixes

--- frontend/components/stream/stream_index.jsx
+++ frontend/components/stream/stream_index.jsx
@@ -7,7 +7,24 @@ class StreamIndex extends React.Component {
 
   render() {
     return(
-      
+      <div className='stream-index'>
+        <ul className='stream-list'>
+          <li>
+            <div className='box'></div>
+          </li>
+          <li>
+            <div className='box'></div>
+          </li>
+          <li>
+            <div className='box'></div>
+          </li>
+          <li>
+            <div className='box'></div>
+          </li>
+        </ul>
+      </div>
     )
   }
 }
+
+export default StreamIndex;
